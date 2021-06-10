4cb4f1a6a5ef9259cfb27ab18c728758c40cdfbb
yuki-inaho <yoshikawa@inaho.co>
2021-06-10 19:44:51 +0900

    Formatting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#coding=utf-8
+# coding=utf-8
 
 import os
 import imageio
@@ -11,22 +11,21 @@ def main(name):
     print(name)
     rim = imageio.imread(name)
     start = time.time()
-    out=pie.PIE(rim)
+    out = pie.PIE(rim)
     end = time.time()
-    cost = (end-start)
-    print('PIE cost: {}'.format(cost))
+    cost = end - start
+    print("PIE cost: {}".format(cost))
     return out
 
 
 if __name__ == "__main__":
     import argparse
     import time
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i','--indir',help='input image sequence list')
-    parser.add_argument('-o','--odir',help='result image name',\
-            default='res')
-    parser.add_argument('-f','--flist',help='specific test sample',\
-            default=0)
+    parser.add_argument("-i", "--indir", help="input image sequence list")
+    parser.add_argument("-o", "--odir", help="result image name", default="res")
+    parser.add_argument("-f", "--flist", help="specific test sample", default=0)
     args = parser.parse_args()
     indir = args.indir
     odir = args.odir
@@ -34,18 +33,17 @@ if __name__ == "__main__":
     names = os.listdir(indir)
     names.sort()
     if flist:
-        with open(flist,'r') as f:
+        with open(flist, "r") as f:
             flines = f.readlines()
-            flines = [ele.strip()\
-                    for ele in flines]
+            flines = [ele.strip() for ele in flines]
     for name in names:
         if flist and (os.path.splitext(name)[0] not in flines):
             continue
-        oname = os.path.join(odir,name)
-        name = os.path.join(indir,name)
+        oname = os.path.join(odir, name)
+        name = os.path.join(indir, name)
         start = time.time()
         res = main(name)
         end = time.time()
-        cost = (end-start)
-        print('total cost: {}'.format(cost))
-        imageio.imwrite(oname.replace('bmp','jpg'),res)
+        cost = end - start
+        print("total cost: {}".format(cost))
+        imageio.imwrite(oname.replace("bmp", "jpg"), res)
