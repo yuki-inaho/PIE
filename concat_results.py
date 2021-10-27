9eb5661bc716ea93b27e33ae86ec578aa85eeceb
yuki-inaho <yoshikawa@inaho.co>
2021-10-27 11:52:34 +0900

    Add a util script

--- concat_results.py
+++ concat_results.py
@@ -13,8 +13,9 @@ SCRIPT_DIR = str(Path().parent)
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="")
-    parser.add_argument("--input-dir", "-i", type=str, default=f"{SCRIPT_DIR}/images")
-    parser.add_argument("--output-dir", "-o", type=str, default=f"{SCRIPT_DIR}/output")
+    parser.add_argument("--raw-input-dir", "-r", type=str, default=f"{SCRIPT_DIR}/images")
+    parser.add_argument("--processed-input-dir", "-p", type=str, default=f"{SCRIPT_DIR}/output")
+    parser.add_argument("--output-dir", "-o", type=str, default=f"{SCRIPT_DIR}/concat")
     return parser.parse_args()
 
 
@@ -25,23 +26,20 @@ def get_image_pathes(input_data_dir: str) -> List[str]:
     return image_path_list
 
 
-def main(image_path: str) -> np.ndarray:
-    print(image_path)
-    rim = cv2.imread(image_path)
-    out = pie.PIE(rim)
-    return out
-
-
 if __name__ == "__main__":
     args = parse_args()
-    image_path_list = get_image_pathes(args.input_dir)
+    raw_image_path_list = get_image_pathes(args.raw_input_dir)
+    processed_image_path_list = get_image_pathes(args.processed_input_dir)
+
     output_dir_pathlib = Path(args.output_dir)
     if output_dir_pathlib.exists():
         shutil.rmtree(str(output_dir_pathlib))
     output_dir_pathlib.mkdir()
 
-    for image_path in tqdm(image_path_list):
-        out = main(image_path)
-        image_name = Path(image_path).name
+    for raw_image_path, processed_image_path in zip(tqdm(raw_image_path_list), processed_image_path_list):
+        image_raw = cv2.imread(raw_image_path)
+        image_processed = cv2.imread(processed_image_path)
+        out = cv2.hconcat([image_raw, image_processed])
+        image_name = Path(raw_image_path).name
         cv2.imwrite(str(output_dir_pathlib.joinpath(image_name)), out)
         cv2.waitKey(10)
